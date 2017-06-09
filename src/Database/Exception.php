d5eefbdd33ec1aa4e24c2fd13b51d30c598e01f7
Robbie Mackay <robbie@ushahidi.com>
2017-06-09 15:14:50 +1200

    Add mysqli driver and basic tests (#2)
    * Add MySQLi Database driver (refs #3898)
    * Add phpunit to dev deps
    * test mysqli connection
    * Remove dependency on Kohana_Exception
    * Update readme to document phpunit

--- src/Database/Exception.php
+++ src/Database/Exception.php
@@ -8,4 +8,31 @@
  * @copyright  (c) 2009 Kohana Team
  * @license    http://kohanaphp.com/license
  */
-class Database_Exception extends Kohana_Exception {}
+class Database_Exception extends Exception {
+
+	/**
+	 * Creates a new translated exception.
+	 *
+	 *     throw new Database_Exception('Something went terrible wrong, :user',
+	 *         array(':user' => $user));
+	 *
+	 * @param   string          $message    error message
+	 * @param   array           $variables  translation variables
+	 * @param   integer|string  $code       the exception code
+	 * @param   Exception       $previous   Previous exception
+	 * @return  void
+	 */
+	public function __construct($message = "", array $variables = NULL, $code = 0, Exception $previous = NULL)
+	{
+		// Set the message
+		$message = empty($variables) ? $message : strtr($message, $variables);
+
+		// Pass the message and integer code to the parent
+		parent::__construct($message, (int) $code, $previous);
+
+		// Save the unmodified code
+		// @link http://bugs.php.net/39615
+		$this->code = $code;
+	}
+
+}
